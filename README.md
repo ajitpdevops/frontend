4153be043834130c82fd5a1743a54fe4169faf82
Ajit Patil <ajit_patil@persistent.com>
2023-03-17 20:48:02 +0530

    Tailwind sample project

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 
 Installation using postcss 
 npm install -D tailwindcss postcss autoprefixer vite
+
+To start the live server here and it would auto reload after changes.
+npm run start
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,12 @@
 module.exports = {
   content: ["*"],
   theme: {
+    screens: {
+      sm: '480px',
+      md: '768px',
+      sm: '976px',
+      sm: '1440px',
+    },
     extend: {},
   },
   plugins: [],
